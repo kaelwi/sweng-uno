9561868926f2a8f92b3ccb347efe5dfa0a0fc8f7
kaelwi <karoline.elisabeth.wild@seznam.cz>
2021-07-06 09:08:24 +0200

    test - some comments (@Before, assertEquals, assertTrue, assertFalse)

--- src/test/CardTest.java
+++ src/test/CardTest.java
@@ -7,6 +7,11 @@ import static org.junit.Assert.assertEquals;
 
 public class CardTest {
 
+    // @Before (annotation for setup if needed)
+    // assertEquals (both in parenthesis have to be same)
+    // assertTrue (return value or value in parenthesis has to be true)
+    // assertFalse (same as above, but it has to be false
+
     @Test
     public void testGetColor() {
         Card card = new Card("R", "0", 0);
